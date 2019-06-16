99d9f4db34490c24ff73175a2dd8aed1def02584
lxz0503 <lxz_20081025@163.com>
2019-06-16 18:39:01 +0800

    add

---
+++ string_process/03_regular_expression.py
@@ -0,0 +1,27 @@
+import re
+
+p = re.compile(r'\d+')
+result = p.split('one1two2three33four4')
+# del result[-1]
+result.pop()
+print(result)
+
+# find number
+
+p = re.compile(r'\d+')
+result = p.findall('one1two2three33four4')
+print(result)
+
+#
+p = re.compile(r'(\w+) (\w+)')
+s = 'I say, hello han xiaoyang'
+result = p.match(s)
+print(result.group(0))
+print(result.group(1))
+print(result.group(2).title())
+
+# sub
+p = re.compile(r'\w+')
+s = 'xy 15 rt 3e,gep'
+result = p.sub('10', s, 2)  # 10是被替换的内容，s是原来的字符串，2表示只替换匹配的前两个
+print(result)   # 10 10 rt 3e,gep
