78581dd0cd51a54360084f4b3e00e0ac6074eab0
lxz0503 <lxz_20081025@163.com>
2019-06-19 11:02:41 +0800

    b

--- xiaozhan/01_pandas_series.py
+++ xiaozhan/01_pandas_series.py
@@ -2,11 +2,40 @@ import pandas as pd
 import numpy as np
 
 
-cities_prices = {'beijing': 60000, 'shanghai': 59000, 'shenzhen': 58000}
-apts = pd.Series(cities_prices)
+cities_prices = {'beijing': 60000, 'shanghai': 59000, 'shenzhen': 58000}  # use dictionary
+list_prices = ['beijing', 'shanghai', 'shenzhen']                 # use list
+apts = pd.Series(cities_prices)  # index is the key of the dictionary
+result = pd.Series(list_prices)
 # print("old values: %s" % apts['shanghai'])
 # print("old values:", apts['shanghai'])
-print(apts)
+# print(apts)
+# print(result)
+arrySer = pd.Series(np.arange(10, 15), index=['a', 'b', 'c', 'd', 'e'])  # use array
+print(arrySer)
+print("the index is %s" % arrySer.index)
+print("the value is %s" % arrySer.values)
+print(arrySer[0:2])
+print(arrySer.iloc[0:2])   # get value from row 0 to 1
+
+print(arrySer[['a', 'b']])
+print(arrySer[['a']])
+print(arrySer.loc[['a', 'b']])    # get value from index a to b
+
+print(arrySer[arrySer.values < 14])  # get elements that value<10
+
+print(arrySer[arrySer.index != 'a'])  # get elements that index!=a
+
+print(arrySer.describe())      # print overall statistic information
+
+print(arrySer.mean())     # 平均值
+print(arrySer.sum())      # 求和
+print(arrySer.value_counts())     # 统计每个数值出现的次数
+
+print(arrySer.drop('a'))     # delete index=a这一行
+
+test = arrySer + arrySer
+print(test)
+
 # print(type(apts))
 # print(apts['beijing'])
 # print(apts[['beijing', 'shanghai']])
@@ -15,11 +44,5 @@ print(apts)
 # print(apts/2)
 # print(np.square(apts))
 
-data = {'city': ['beijing', 'shanghai', 'shenzhen', 'guangzhou'],
-        'year': [2016, 2017, 2018, 2019],
-        'house_price': [50000, 48000, 35000, 30000]}
-data_frame = pd.DataFrame(data, columns=['year', 'city', 'house_price'])
-# print(data_frame)
-print(data_frame.values)
-print(data_frame.values[0])
-print(data_frame.values[0][2])
+
+
