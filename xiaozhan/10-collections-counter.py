c1a3953f1b091cdcf9fae1b56e8c34b9eaca9e7d
lxz0503 <lxz_20081025@163.com>
2019-07-03 20:07:28 +0800

    bb

--- xiaozhan/10-collections-counter.py
+++ xiaozhan/10-collections-counter.py
@@ -4,14 +4,14 @@ import collections
 
 test_str = "hello world"
 count_dict = dict(collections.Counter(test_str))
-print(count_dict)
+#print(count_dict)
 
 # 统计字符串中出现次数最多的字符
 
 from collections import Counter
 
-# s = 'helloworldabbbaaaccc'
-s = input()                     # 从控制台输入字符串
+s = 'helloworldabbbaaaccc'
+#s = input()                     # 从控制台输入字符串
 c = Counter(s)
 print(c.most_common(5))         # 获取频率最高的5个字符，存储在列表
 print(c.most_common(5)[0][0])   # 打印列表第一个元素
