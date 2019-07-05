8b1336a2b58f504fae6e7201e2a8ec26f665ba4c
lxz0503 <lxz_20081025@163.com>
2019-07-05 16:52:06 +0800

    aa

--- xiaozhan/aaaaaaaaaaa.py
+++ xiaozhan/aaaaaaaaaaa.py
@@ -221,5 +221,22 @@ def two_sum(numbers, target):
 # print(type(ret))
 print(two_sum([1,2,3,4], 5))
 
+# lambda
+# x 是形参，冒号后面是函数体
+func = lambda x: x+1
+print(func(10))
+
+name = "aaaa"
+func = lambda x: x + "_sb"
+res = func(name)
+print("lambda result is", res)
+
+func = lambda x, y, z: (x+1, y+1, z+1)
+res = func(1, 2, 3)   # return is tuple
+print("the result is", res)
+print(type(res))
+
+
+
 
 
