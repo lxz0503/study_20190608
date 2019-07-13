a2f9a4c8f8e64c8026b913ea1bc86f9cf984f1cd
lxz0503 <lxz_20081025@163.com>
2019-07-13 22:44:57 +0800

    20

--- xiaozhan/day-20.py
+++ xiaozhan/day-20.py
@@ -3,38 +3,36 @@
 # 应用场景，例如, 不能修改函数体
 # 装饰器 = 高阶函数 + 函数嵌套 + 闭包
 # 带固定参数的装饰器
-import time
-
-def deco(f):
-    def wrapper(a,b):
-        start_time = time.time()
-        f(a,b)
-        end_time = time.time()
-        execution_time = (end_time - start_time)*1000
-        print("time is %d ms" % execution_time)
-    return wrapper
-
-@deco
-def f(a,b):
-    print("be on")
-    time.sleep(1)
-    print("result is %d" %(a+b))
-
-if __name__ == '__main__':
-    f(3,4)
+# import time
+#
+# def deco(f):                # 以函数名作为参数
+#     def wrapper(a,b):       # 函数嵌套
+#         start_time = time.time()
+#         f(a,b)
+#         end_time = time.time()
+#         execution_time = (end_time - start_time)*1000
+#         print("time is %d ms" % execution_time)
+#     return wrapper          # 返回值是嵌套函数的函数名
+#
+# @deco
+# def f(a,b):
+#     print("be on")
+#     time.sleep(1)
+#     print("result is %d" %(a+b))   # 执行完这一步，跳转到def wrapper(a,b)里面的f(a,b)
+#
+# if __name__ == '__main__':
+#     f(3,4)            # 此处设置断点，查看函数如何执行,顺序是先执行deco()函数，在执行f(a,b)之前跳转到def f(a,b)函数里面
 
 # 无固定参数的装饰器
 import time
-
 def deco(f):
     def wrapper(*args, **kwargs):
         start_time = time.time()
         f(*args, **kwargs)
         end_time = time.time()
-        execution_time_ = (end_time - start_time)*1000
-        print("time is %d ms" %execution_time)
+        execution_time = (end_time - start_time) * 1000
+        print("time is %d ms" % execution_time)
     return wrapper
-
 
 @deco
 def f(a,b):
@@ -47,8 +45,7 @@ def f2(a,b,c):
     print("be on")
     time.sleep(1)
     print("result is %d" %(a+b+c))
-
-
+#
 if __name__ == '__main__':
     f2(3,4,5)
     f(3,4)
