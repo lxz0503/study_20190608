1f2d10a9b755079e16fee1b71b633bc4209cf775
lxz0503 <lxz_20081025@163.com>
2019-07-14 10:35:41 +0800

    update

--- 01_Python基础/hm_11_元组和字典的拆包.py
+++ 01_Python基础/hm_11_元组和字典的拆包.py
@@ -1,8 +1,8 @@
 def demo(*args, **kwargs):
-    print(args)
-    print(kwargs)
+    print(args)     # (1, 2, 3)
+    print(kwargs)   # {'name': '小明', 'age': 18}
 
 
-gl_nums = (1, 2, 3)
+gl_nums = [1, 2, 3]   # gl_nums = (1, 2, 3)
 gl_dict = {"name": "小明", "age": 18}
 demo(*gl_nums, **gl_dict)
