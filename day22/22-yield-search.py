7906f4a0bd6022f56b260d87796d09e83d6261c5
lxz0503 <lxz_20081025@163.com>
2019-07-18 22:13:44 +0800

    search index

---
+++ day22/22-yield-search.py
@@ -0,0 +1,48 @@
+# 迭代是Python最强大的功能之一，是访问集合元素的一种方式。
+# 迭代器是一个可以记住遍历的位置的对象。
+# 迭代器对象从集合的第一个元素开始访问，直到所有的元素被访问完结束。
+# 迭代器只能往前不会后退。
+# 迭代器有两个基本的方法：iter() 和 next()。
+# 字符串，列表或元组对象都可用于创建迭代器
+
+l = [1, 2, 3, 4]
+it = iter(l)    # 创建迭代器对象
+# print(type(it))
+# print(next(it))
+# 迭代器对象可以使用常规for语句进行遍历
+# for x in it:
+    # print(x, end="\t")
+
+# 在 Python 中，使用了 yield 的函数被称为生成器（generator）。
+# 跟普通函数不同的是，生成器是一个返回迭代器的函数，只能用于迭代操作，
+# 更简单点理解生成器就是一个迭代器。
+# 在调用生成器运行的过程中，每次遇到 yield 时函数会暂停并保存当前所有的运行信息，返回 yield 的值,
+# 并在下一次执行 next() 方法时从当前位置继续运行。
+# 调用一个生成器函数，返回的是一个迭代器对象。
+
+# 有一个序列，查找里面数字10的下标，用迭代器实现
+def func(l):
+    for i in l:
+        yield i
+
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+res = func(l)     # 迭代器对象
+i = 0    # 全局变量记录下标
+for j in res:
+    if j != 10:
+        i = i + 1
+        continue
+    else:
+        print("the index of 10 is %d" % i)  # the index is 9
+
+# another method
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+i = 0
+for j in l:
+    if j != 10:
+        i = i + 1
+    else:
+        break
+print("the index is %d" % i)    # the index is 9
+#
