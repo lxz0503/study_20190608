b56c44cc09e56ce418c02f01e920c2f1b2998c70
lxz0503 <lxz_20081025@163.com>
2019-07-21 19:33:53 +0800

    file op

--- 01_Python基础/hm_16_文件操作.py
+++ 01_Python基础/hm_16_文件操作.py
@@ -13,7 +13,8 @@ file_name = "file_test"
 with open(file_name, "r") as f:
     content = f.read()
 print(content)   # 此时需要处理字符串文本
-
+f_list = content.split("\n")
+print(f_list)        # ['aaaaa', 'bbbbb', 'ccccc']
 #
 with open("test_file.txt", "r") as f:
     # content = f.read()
