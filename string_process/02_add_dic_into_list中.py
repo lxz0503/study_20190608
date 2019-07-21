f226c2c5cbcfc459ee33339a3050c0e09e276e44
lxz0503 <lxz_20081025@163.com>
2019-07-21 22:44:10 +0800

    dic

--- string_process/02_add_dic_into_list中.py
+++ string_process/02_add_dic_into_list中.py
@@ -1,8 +1,9 @@
 counts = {'lixiaozhan': 2, 'beijing': 2, 'jiaotong': 1, 'university': 3, 'shanghai': 3, 'zhangsan': 1, 'nanjing': 2}
 items = list(counts.items())
-print(items)
+print(items)   # [('lixiaozhan', 2), ('beijing', 2), ('jiaotong', 1), ('university', 3), ('shanghai', 3), ('zhangsan', 1), ('nanjing', 2)]
 items.sort(key=lambda x: x[1], reverse=True)  # 用list内置的sort方法按照第二个元素排序,逆序
 print("the sorted list is %s" % items)
+# the sorted list is [('university', 3), ('shanghai', 3), ('lixiaozhan', 2), ('beijing', 2), ('nanjing', 2), ('jiaotong', 1), ('zhangsan', 1)]
 
 for i in range(5):
     word, count = items[i]     # 每个list元素是一个元组
