af7b2eb075eaf387ccab2ead3ebaac2474caa4fe
lxz0503 <lxz_20081025@163.com>
2019-07-31 12:55:05 +0800

    up

--- python_面试题/3-列表-字符串-转换.py
+++ python_面试题/3-列表-字符串-转换.py
@@ -13,12 +13,19 @@ print(type(s))
 new_s = s.split(',')
 print(new_s)            # ['1', ' 2', ' 3']
 
+new_s = "1,2,3".split(',')
+print("just one line to a str list", new_s)      # ['1', '2', '3']
+
 n_s = []
 for i in new_s:
     n_s.append(int(i))
 print(n_s)              # [1, 2, 3]
 
+n_s = [int(x) for x in ['1','2','3']]
+print("just one line code", n_s)
+
 b = [(1,), (2,), (3,)]
 for i in b:
     # print(type(i))
     print(i)      # (1,) (2,) (3,)
+
