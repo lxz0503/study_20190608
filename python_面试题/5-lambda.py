cfb971ced7b354812a4dbeef768ed108714b7cd5
lxz0503 <lxz_20081025@163.com>
2019-07-31 14:22:41 +0800

    lambda

--- python_面试题/5-lambda.py
+++ python_面试题/5-lambda.py
@@ -9,9 +9,9 @@
 temp = lambda x,y:x+y
 print(temp(4, 10))
 
-    # 如何用一行代码生成[1, 4, 9, 16, 25, 36, 49, 64, 81, 100] ?
+# 如何用一行代码生成[1, 4, 9, 16, 25, 36, 49, 64, 81, 100] ?
 
-s = [i for i in range(1,101)]
+s = [i * i for i in range(1,11)]
 print(s)
 
 l1 = (('a'), ('b'))
@@ -19,3 +19,17 @@ l2 = (('c'), ('d'))
 print(list(dict(zip(l1,l2)).items()))       # [('a', 'c'), ('b', 'd')]
 print([{n[0]:n[1]} for n in zip(l1,l2)])    # [{'a': 'c'}, {'b': 'd'}]
 
+# lambda
+# 冒号前面的参数是形参，冒号后面是函数体
+func = lambda x: x+1
+print(func(10))
+
+name = "aaaa"
+func = lambda x: x + "_sb"
+res = func(name)
+print("lambda result is", res)
+
+func = lambda x, y, z: (x+1, y+1, z+1)
+res = func(1, 2, 3)   # return is tuple
+print("the result is", res)
+print(type(res))
