43892e13d5df996456eee3822d0625b13a6da05f
lxz0503 <lxz_20081025@163.com>
2019-08-02 15:33:51 +0800

    str to byte

--- 01_Python基础/hm_18_subprocess练习.py
+++ 01_Python基础/hm_18_subprocess练习.py
@@ -1,13 +1,24 @@
+# 字节转化为字符串用下面的两种都可以
+
+print(str(b'example', encoding='utf-8'))    # example
+print(bytes.decode(b'example'))
+
+# 字符串转化为字节,用下面两种方法都可以
+print(bytes('example', encoding='utf-8'))     # b'example'
+print(str.encode('example'))                  # b'example'
+
 import subprocess
 
+def run_cmd(command):
+    # ret = subprocess.Popen(bytes(command, encoding='utf-8').decode('utf-8'),   # 字符串参数要转换为字节类型
+    ret = subprocess.Popen(str.encode(command).decode('utf-8'),   # 字符串参数要转换为字节类型
+                           shell=True,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+    stdout = ret.stdout.read()
+    stderr = ret.stderr.read()
+    return str(stdout, encoding='utf-8')
 
-def runcmd(command):
-    ret = subprocess.run(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8",timeout=1)
-    if ret.returncode == 0:
-        print("success:",ret)
-    else:
-        print("error:",ret)
 
-
-runcmd(["dir", r"D:\\"]) # 序列参数
-runcmd("exit 1")   # 字符串参数
+result = run_cmd("dir")     # 传入的参数是一个字符串
+print("命令执行结果是",  result)
