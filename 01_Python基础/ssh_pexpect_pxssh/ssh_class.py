6e7188c6c01c59be58052621ef10f27ed4e3afbb
lxz0503 <lxz_20081025@163.com>
2019-08-07 15:22:16 +0800

    ssh pexpect pxssh spawn

---
+++ 01_Python基础/ssh_pexpect_pxssh/ssh_class.py
@@ -0,0 +1,69 @@
+"""this is for ssh test """
+
+# !/usr/bin/env python
+# encoding = utf-8
+
+import sys, re
+from pexpect import pxssh
+class SshTest(object):
+    def __init__(self, host_name, user_name, password):
+        self.host_name = host_name
+        self.user_name = user_name
+        self.password = password
+        self.s = None
+
+    def connect(self):
+        try:
+            s = pxssh.pxssh(timeout=60*60)
+            s.login(server=self.host_name, username=self.user_name, password=self.password)
+            #f = open("logfile.txt",'w')  # record test log to a file
+            #s.logfile = f   
+            s.logfile = sys.stdout    
+            self.s = s     # 
+        except pxssh.ExceptionPxssh as e:
+            print(e)
+
+    def disconnect(self):
+        self.s.logout()
+        
+
+    #
+    def send_cmd(self, runcmd):
+        self.s.sendline(runcmd)
+        self.s.prompt()
+        print(self.s.before)
+        return self.s.before
+    
+    def mv_file(self, src, dst):
+        self.s.sendline('mv %s %s' % (src, dst))
+        self.s.prompt()
+        print(self.s.before)
+        #return self.s.before
+
+    def set_env(self, dvd_path):
+        self.s.sendline("cd %s" % dvd_path)
+        self.s.prompt()
+        self.s.sendline("./wrenv.sh -p helix")
+        self.s.prompt()
+
+    def run_script(self, ip, port):
+        self.s.sendline('python parameter-getopt.py -h -i %s -p %d 2>&1 | tee test.log' % (ip, port))
+        self.s.prompt()
+        res = self.s.before
+        print(res)
+
+
+if __name__ == "__main__":
+    ssh_config = {'host_name':'128.224.163.8', 'username':'windriver', 'password':'windriver'}
+    cmd = ['uname -a', 'cat /proc/version']
+    src_file = "/home/windriver/1.txt"
+    dst_file = "/home/windriver/2.txt"
+    #cmd = "mv " + src_file + " " + dst_file     # 
+    ssh = SshTest(ssh_config['host_name'], ssh_config['username'], ssh_config['password'])
+    ssh.connect()
+    for i in cmd:
+        ssh.send_cmd(i)
+    ssh.run_script("10.0.0.1", 80)
+    #ssh.mv_file(src_file, dst_file)
+    ssh.disconnect()
+
---
+++ 01_Python基础/ssh_pexpect_spawn_interactive/ssh_spawn.py
@@ -0,0 +1,92 @@
+"""this is for ssh test with spawn """
+# if you want to ssh to another server and run some interactive command like scp, ftp
+# you must use spawn but not pxssh
+
+# !/usr/bin/env python
+# encoding = utf-8
+
+import sys, re
+from pexpect import pxssh
+import pexpect
+class SshTest(object):
+    def __init__(self, host_name, user_name, password):
+        self.host_name = host_name
+        self.user_name = user_name
+        self.password = password
+        self.s = None
+
+    def connect(self):
+        try:
+            cmdSSH = 'ssh windriver@128.224.163.8'
+            s = pexpect.spawn(cmdSSH)
+            s.logfile = sys.stdout
+            #f = open("spawn.log",'w')
+            #sys.stdout = f
+            for i in range(2):
+                i = s.expect(['windriver@PEK-QCAO1-D2:~$',
+                                 'assword:',
+                                 'Are you sure you want to continue connecting',
+                                 pexpect.TIMEOUT,
+                                 pexpect.EOF]) 
+                if i == 2:
+                    s.sendline('yes')
+                if i == 1:
+                    s.sendline("windriver")
+                    s.expect('windriver@PEK-QCAO1-D2:~')
+                    break
+                if i == 0:
+                    break
+                if i == 3 or i == 4:
+                    print("ERROR!: cannot ssh to server!")
+                    break
+        except Exception:
+            print("exception")
+        self.s = s
+
+    def disconnect(self):
+        self.s.close()
+
+    def send_cmd(self, runcmd):
+        self.s.sendline(runcmd)
+        self.s.expect("windriver@PEK-QCAO1-D2:~")
+        #print(self.s.before)
+    # 
+    def transfer_file(self, src, dst):
+        self.s.sendline('scp %s %s' % (src, dst))
+        for i in range(2):
+            i = self.s.expect(['assword:', 'Are you sure you want to continue connecting',pexpect.TIMEOUT]) 
+            if i == 1:
+                self.s.sendline('yes')
+            if i == 0:
+                self.s.sendline("3333xli3")
+                self.s.expect("00:00")
+                break
+            if i == 2:
+                print("copy log timeout!")
+                break
+    def mv_file(self, src, dst):
+        self.s.sendline('mv %s %s' % (src, dst))
+        self.s.expect("windriver@PEK-QCAO1-D2:~")
+
+    def run_script(self, ip, port):
+        self.s.sendline('python parameter-getopt.py -h -i %s -p %d 2>&1 | tee test.log' % (ip, port))
+        self.s.expect("windriver@PEK-QCAO1-D2:~")
+
+
+if __name__ == "__main__":
+    ssh_config = {'host_name':'128.224.163.8', 'username':'windriver', 'password':'windriver'}
+    cmd = ['uname -a', 'cat /proc/version']
+    src_file = "/home/windriver/1.txt"
+    dst_file = "/home/windriver/2.txt"
+    #cmd = "mv " + src_file + " " + dst_file     # 
+    ssh = SshTest(ssh_config['host_name'], ssh_config['username'], ssh_config['password'])
+    ssh.connect()
+    for i in cmd:
+        ssh.send_cmd(i)
+    ssh.run_script("10.0.0.1", 80)
+    ssh.mv_file(src_file, dst_file)
+    src = "/home/windriver/2.txt"
+    dst = "xli3@128.224.156.156:/workspace/share/kong-05/xli3/xiaozhan_python"
+    ssh.transfer_file(src, dst)
+    ssh.disconnect()
+
