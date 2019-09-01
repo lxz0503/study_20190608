07fdbb8c27fd1a6f577462da29fbecc9aecb988d
lxz0503 <lxz_20081025@163.com>
2019-09-01 16:49:27 +0800

    python data type

--- python_面试题/13-remove-duplicate.py
+++ python_面试题/13-remove-duplicate.py
@@ -1,20 +1,19 @@
-# def removeDuplicates(nums):
+# def removeDuplicates(nums):    # 最快捷，但是创建了新的序列，占用内存
 #     tmp = set(nums)
 #     tmp = list(tmp)
 #     return len(tmp)
 
 def removeDuplicates(nums):
     i = 0
-    while i < len(nums) - 1:    # 从第一个元素开始循环到倒数第二个元素
-        if nums[i] == nums[i + 1]:
-            # nums.pop(i)    # 每次删除一个第一个相同的元素，然后回到while循环，从第一个元素开始再次比较
+    while i < len(nums) - 1:        # 从第一个元素开始循环到倒数第二个元素
+        if nums[i] == nums[i + 1]:  # 当前元素和下一个元素比较
+            # nums.pop(i)           # 每次删除一个第一个相同的元素，然后回到while循环，从第一个元素开始再次比较
             nums.remove(nums[i])    # 每次删除一个第一个相同的元素，然后回到while循环，从第一个元素开始再次比较
         else:
             i += 1
-    return len(nums)
+    return len(nums)               # 返回删除重复元素后的序列长度
 
 
-
-nums = [0,0,1,1,1,2,2,3,3,4]
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 res = removeDuplicates(nums)
 print(res)
--- xiaozhan/aaaaaaaaaaa.py
+++ xiaozhan/aaaaaaaaaaa.py
@@ -210,21 +210,6 @@ res = func(1, 2, 3)   # return is tuple
 print("the result is", res)
 print(type(res))
 
-# map() 会根据提供的函数对指定序列做映射。
-# 第一个参数 function 以参数序列中的每一个元素调用 function 函数，返回包含每次 function 函数返回值的新列表
-
-def square(x):  # 计算平方
-    return x ** 2
-
-map(square, [1, 2, 3, 4, 5])  # 计算列表各个元素的平方
-# [1, 4, 9, 16, 25]
-map(lambda x: x ** 2, [1, 2, 3, 4, 5])  # 使用 lambda 匿名函数
-#[1, 4, 9, 16, 25]
-
-# 提供了两个列表，对相同位置的列表数据进行相加
-map(lambda x, y: x + y, [1, 3, 5, 7, 9], [2, 4, 6, 8, 10])
-# [3, 7, 11, 15, 19]
-
 #
 print(abs(-2))
 
