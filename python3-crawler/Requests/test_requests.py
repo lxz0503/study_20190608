a0e23da8cab47c8ccd7bed0e6d9b08c14a2c1e6d
lxz0503 <lxz_20081025@163.com>
2019-09-20 11:11:13 +0800

    usage of requests lib

--- python3-crawler/Requests/test_requests.py
+++ python3-crawler/Requests/test_requests.py
@@ -22,7 +22,8 @@ requests.options('http://httpbin.org/get')
 #
 result = requests.get('http://httpbin.org/get')
 print('start')
-print(result.text)
+print(result.text)     # retrun is json
+# print(result.content)    # return is bytes
 print('end')
 
 # request with parameters
@@ -43,6 +44,7 @@ print(result.json())
 print('json end')
 
 # download image and save it, image is binary code
+
 result = requests.get('http://pic25.nipic.com/20121112/9252150_150552938000_2.jpg')
 with open('test_image.jpg', 'wb') as f:
     f.write(result.content)
@@ -52,7 +54,7 @@ headers = {
           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
           }
 result = requests.get('https://www.zhihu.com/explore', headers=headers)
-print(result.text)
+print(result.content)
 
 # request with post method
 form_data = {
@@ -74,12 +76,14 @@ result = requests.get('https://wwww.baidu.com')
 exit() if result.status_code != 200 else print('request successfully')
 
 # upload files or image
+print('start to upload image')
 files = {'file': open('test_image.jpg', 'rb')}
 headers = {
           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
           }
 result = requests.post('http://httpbin.org/post', headers=headers, files=files)
 print(result.text)
+print('end to upload image')
 
 # get cookies
 result = requests.get('https://wwww.baidu.com')
@@ -87,10 +91,51 @@ print(result.cookies)
 for key,value in result.cookies.items():
     print(key + '=====' + value)
 
-#
-requests.get('http://httpbin.org/cookies/set/number/12345')
+# session, this can not get cookies because in the same browser
+requests.get('http://httpbin.org/cookies/set/number/123456789')
 result = requests.get('http://httpbin.org/cookies')
 print(result.text)
+# {
+#   "cookies": {}
+# }
+
+# this can get correct cookies
+s = requests.Session()
+s.get('http://httpbin.org/cookies/set/number/123456789')
+result = s.get('http://httpbin.org/cookies')
+print(result.text)
+# {
+#   "cookies": {
+#     "number": "123456789"
+#   }
+# }
+
+# certificate
+result = requests.get('https://www.12306.cn')
+print(result.status_code)
+# set proxy
+proxies = {
+    'http': 'http://127.0.0.1:9743',
+    'https': 'https://127.0.0.1:9743',
+}
+# result = requests.get('https://www.12306.cn', proxies=proxies)
+
+# authentication
+# from requests.auth import HTTPBasicAuth
+
+# result = requests.get('http:120.27.34.24:9001', auth=('user', '123'))
+
+# exception
+from requests.exceptions import ReadTimeout,HTTPError,RequestException
+try:
+    result = requests.get('http://www.123.com', timeout=1)
+except ReadTimeout:
+    print('timeout')
+except HTTPError:
+    print('http error')
+finally:
+    print(result.status_code)
+
 
 
 
