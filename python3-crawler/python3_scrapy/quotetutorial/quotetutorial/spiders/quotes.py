0f39f9d010c7ec8c616739f24070c7d9032c8722
lxz0503 <lxz_20081025@163.com>
2019-09-26 22:55:09 +0800

    scrapy entry

---
+++ python3-crawler/python3_scrapy/quotetutorial/quotetutorial/spiders/quotes.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        print('the web content is:')
+        print(response.text)
