2dd6a0cfed44a45821e5a4a7ba06d1827d00341e
lxz0503 <lxz_20081025@163.com>
2019-12-03 15:36:09 +0800

    update linux command

--- python3-crawler/environment/crawler-tools.py
+++ python3-crawler/environment/crawler-tools.py
@@ -39,8 +39,14 @@ import pymongo
 
 client = pymongo.MongoClient('localhost')
 db = client['xiaozhan']           # create database name is xiaozhan
-db['performance'].insert({'name': 'tcp'})      # table is performance and insert values
-r = db['performance'].find_one({'name': 'tcp'})   # use xiaozhan, show collections, db.performance.find().pretty()
+insert_data = {"board": "board_name", "TCP": 64}
+myquery = {"board": "IA"}
+# db['performance']    this is to create a table
+# db['performance'].insert(insert_data)
+db['performance'].insert({'name': 'tcp'})      # table is performance and insert values,the value is a dictionary
+# r = db['performance'].find_one({'name': 'tcp'})   # use xiaozhan, show collections, db.performance.find().pretty()
+r = db['performance'].find_one(myquery)   # use xiaozhan,
+
 print(r)
 #
 # import redis
@@ -49,4 +55,20 @@ print(r)
 # result = r.get('name')
 # print(result)
 
-#
+#!/usr/bin/python3
+
+# import pymongo
+
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["runoobdb"]
+# mycol = mydb["sites"]
+
+mylist = [
+    {"name": "Taobao", "alexa": "100", "url": "https://www.taobao.com"},
+    {"name": "QQ", "alexa": "101", "url": "https://www.qq.com"},
+    {"name": "Facebook", "alexa": "10", "url": "https://www.facebook.com"},
+    {"name": "??", "alexa": "103", "url": "https://www.zhihu.com"},
+    {"name": "Github", "alexa": "109", "url": "https://www.github.com"}
+]
+
+mycol.insert_many(mylist)
