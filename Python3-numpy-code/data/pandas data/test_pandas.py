5c672ea05de38640d5bd3b5a45746d347729d946
lxz0503 <lxz_20081025@163.com>
2019-12-30 21:19:08 +0800

    win7 is ok but win10 at company is not ok

--- Python3-numpy-code/data/pandas data/test_pandas.py
+++ Python3-numpy-code/data/pandas data/test_pandas.py
@@ -5,5 +5,7 @@ import os
 
 f_dir = os.path.dirname(__file__) + '/taobao_data.csv'
 df = pd.read_csv(f_dir, delimiter=',', encoding='utf-8', header=0)
-
 print(df)
+
+# 以上代码在公司win10英文版系统平台运行不能通过，由于编码问题，暂时没解决
+# 在家里win7中文版系统，顺利运行通过
