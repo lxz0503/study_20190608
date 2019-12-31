2ed0421282543e9f0b1d1edfa09253413bf48cc4
lxz0503 <lxz_20081025@163.com>
2019-12-31 10:34:13 +0800

    update data

--- Python3-numpy-code/data/pandas data/test_pandas.py
+++ Python3-numpy-code/data/pandas data/test_pandas.py
@@ -1,9 +1,9 @@
 #! /user/bin/env python3
-# coding=utf-8
+
 import pandas as pd
 import os
 
 f_dir = os.path.dirname(__file__) + '/taobao_data.csv'
-df = pd.read_csv(f_dir, delimiter=',', encoding='utf-8', header=0)
+df = pd.read_csv(f_dir, delimiter=',', encoding='gb2312', header=0)
 
 print(df)
