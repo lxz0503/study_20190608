baf6064f951879331ee22c92ece7939ace2abbaa
lxz0503 <lxz_20081025@163.com>
2019-12-31 16:15:35 +0800

    python3 tips

--- Python3-numpy-code/data/pandas data/test_pandas.py
+++ Python3-numpy-code/data/pandas data/test_pandas.py
@@ -1,14 +1,16 @@
 #! /user/bin/env python3
 # coding=utf-8
+# 如果有中文字符，注意设置pycharm软件的编码为UTF-8即可,并设置文件编码为UTF-8
+
 import pandas as pd
 import os
 import codecs
 
 f_dir = os.path.dirname(__file__) + '/taobao_data.csv'
 # df = pd.read_csv(f_dir, delimiter=',', encoding='gb2312', header=0)
-df = pd.read_csv(f_dir, delimiter=',', encoding='utf-8', header=0)
+df = pd.read_csv(f_dir, delimiter=',', encoding='utf8', header=0)
 print(df)
 
+# 向csv写入数据
+df.to_csv('taobao_price_data_xiaozhan.csv', columns=['商品', '价格'], index=True, header=True)
 
-# 以上代码在公司win10英文版系统平台运行不能通过，由于编码问题，暂时没解决
-# 在家里win7中文版系统，顺利运行通过
