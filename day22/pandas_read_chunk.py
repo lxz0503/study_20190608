679ee99cf5398f261fc487632b55b5fcdffcce27
lxz0503 <lxz_20081025@163.com>
2020-02-29 21:03:26 +0800

    pandas data to list

--- day22/pandas_read_chunk.py
+++ day22/pandas_read_chunk.py
@@ -1,14 +1,17 @@
 #!/usr/bon/env python3
 # coding=utf-8
 # Panda read_csv()把第一行的数据变成了列名，怎么处理
+# pandas DataFrame数据转为list
+# 使用np.array()函数把DataFrame转化为np.ndarray()，再利用tolist()函数把np.ndarray()转为list
 # this is to read a large file with pandas
 # 每次读取5行
 
 import pandas as pd
+import numpy as np
 
 def pandas_read(filename, sep=',', size=5):
     # names参数可以决定列名,否则就会以第一列的名字默认作为列名
-    reader = pd.read_csv(filename, sep, chunksize=size, header=None, names=['test result'])
+    reader = pd.read_csv(filename, sep, chunksize=size, header=None, names=['test result'])   # pd.dataframe
     while True:
         try:
             yield reader.get_chunk()
@@ -23,5 +26,8 @@ if __name__ == '__main__':
     #     print(c)
     print('case name is {}'.format((next(g).values[0][0].split(':')[0])))
     print('case status is {}'.format((next(g).values[0][0].split(':')[1])))
-    print(type(next(g).values[0]))
-    print(next(g).values[0])
+    print(type(next(g).values[0]))    # np.ndarray()   <class 'numpy.ndarray'>
+    # print(next(g).values[0])
+    print(np.array(next(g).values[0]).tolist())
+    print('case status is {}'.format(np.array(next(g).values[0]).tolist()[0].split(':')[1]))
+
