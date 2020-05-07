a86c73cee167c5c8929479e2d36835404b5ccfcd
lxz0503 <lxz_20081025@163.com>
2020-05-07 22:32:52 +0800

    psutil memory usage

---
+++ 01_Python_Basics/63-memory-psutil.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# this can display memory usage
+
+def bytes2human(n):
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.1f%s' % (value, s)
+    return '%sB' % n
+
+if __name__ == '__main__':
+    r = bytes2human(33811652608987)
+    print(r)
+
