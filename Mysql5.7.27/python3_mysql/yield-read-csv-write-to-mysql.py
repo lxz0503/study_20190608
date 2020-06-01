e72345c9fe1b7241ab0c6e42907451e9ec033579
lxz0503 <lxz_20081025@163.com>
2020-06-01 19:27:15 +0800

    yiled read csv and write it into mysql

--- Mysql5.7.27/python3_mysql/yield-read-csv-write-to-mysql.py
+++ Mysql5.7.27/python3_mysql/yield-read-csv-write-to-mysql.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+# coding=utf-8
 import pymysql
 import time
 from python3_mysql.operations import *
+import csv
+from collections import namedtuple
 
 class DatabaseInit(object):
     def __init__(self, host, dbname, username, password, charset):
@@ -46,6 +49,15 @@ class DatabaseInit(object):
             self.conn.commit()    # do not forget to commit after modification
             self.disconnect_db()
             print('create database and table ok')
+
+    @staticmethod
+    def get_data(file_name):
+        with open(file_name) as f:
+            f_csv = csv.reader(f)
+            headings = next(f_csv)
+            Row = namedtuple('Row', headings)
+            for r in f_csv:
+                yield Row(*r)
 
     def insert_data(self, insert_data, args):
         try:
@@ -110,7 +122,11 @@ if __name__ == '__main__':
                       charset='utf8')      # 家里的win7只能用gbk编码,utf8 is for company
     db.connect_db()
     db.create()
-    db.insert_data(insert_data, args)
+    # xiaozhan debug, auto generate id.
+    for i, t in enumerate(db.get_data('data.csv'), 1):
+        args = [(i, t.name, t.status, t.arch, t.sprint)]
+        db.insert_data(insert_data, args)
+    # xiaozhan debug
     db.search_data()
     # this can put failed cases into a dictionary
     res = db.search_data()
