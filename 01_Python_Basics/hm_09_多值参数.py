93b8073fc3e891f134ff5c92e2ee751a9fba2a9f
lxz0503 <lxz_20081025@163.com>
2020-08-18 17:24:51 +0800

    Jenkins simple pipeline

--- 01_Python_Basics/hm_09_多值参数.py
+++ 01_Python_Basics/hm_09_多值参数.py
@@ -1,5 +1,5 @@
 # python有两种多值参数
-# 参数名前增加一个*,可以接收元组
+# 参数名前增加一个*,可以接收元组,列表，或者一串字符
 # 参数名前增加两个*,可以接收字典
 
 
@@ -9,4 +9,5 @@ def demo(num, *args, **kwargs):
     print(kwargs)    # {'name': 'xiaoming', 'age': 18, 'gender': True}
 
 
-demo(1, 2, 3, 4, 5, name="xiaoming", age=18, gender=True)
+if __name__ == '__main__':
+    demo(1, 2, 3, 4, 5, name="xiaoming", age=18, gender=True)
--- 01_Python_Basics/hm_11_元组和字典的拆包.py
+++ 01_Python_Basics/hm_11_元组和字典的拆包.py
@@ -1,8 +1,10 @@
+
 def demo(*args, **kwargs):
     print(args)     # (1, 2, 3)
     print(kwargs)   # {'name': '小明', 'age': 18}
 
 
-gl_nums = [1, 2, 3]   # gl_nums = (1, 2, 3)
-gl_dict = {"name": "小明", "age": 18}
-demo(*gl_nums, **gl_dict)
+if __name__ == '__main__':
+    gl_nums = [1, 2, 3]        # gl_nums = (1, 2, 3)
+    gl_dict = {"name": "小明", "age": 18}
+    demo(*gl_nums, **gl_dict)     # 如果传递的参数是序列，在前面加上*， 如果传递的是字典格式参数，则添加两个**
