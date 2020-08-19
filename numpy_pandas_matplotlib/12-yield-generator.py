75ea75eed9a87d6268993f1d4cb37be9a0892bbe
lxz0503 <lxz_20081025@163.com>
2020-08-19 21:40:16 +0800

    Automation foundation

--- numpy_pandas_matplotlib/12-yield-generator.py
+++ numpy_pandas_matplotlib/12-yield-generator.py
@@ -8,11 +8,8 @@ print(l)
 
 # 文件中最长的那一行的长度，去掉空格
 
-def func():
-    return max(len(x.strip()) for x in open("population_statistics"))
-
-res = func()
-print(res)
+res = max(len(x.strip()) for x in open("population_statistics"))
+print('the max len is %s' % res)
 
 # 如果列表内容特别大，就用生成器表达式，把[]替换为()即可,这样不会真用很大内存
 
@@ -22,14 +19,14 @@ print(l.__next__())   # 这里输出0
 print(l.__next__())   # 这里输出1
 
 # yield相当于函数里面的return
-
-def test():
-    print("first")
-    yield "the first"
-    print("second")
-    yield 2
-    print("third")
-    yield 3
-res = test()
-print(res.__next__())   # 这里输出1
-print(res.__next__())   # 这里输出2
+#
+# def test():
+#     print("first")
+#     yield "the first"
+#     print("second")
+#     yield 2
+#     print("third")
+#     yield 3
+# res = test()
+# print(res.__next__())   # 这里输出1
+# print(res.__next__())   # 这里输出2
